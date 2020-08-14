79b46522161b342db2a1d5e1530a08854a4b96e4
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2020-08-14 12:19:58 +0200

    Update downloader

--- codeLF/download.sh
+++ codeLF/download.sh
@@ -3,6 +3,6 @@
 TODOWNLOAD="AnalysisResults.root TOFPid.root TPCPid.root"
 
 for i in $TODOWNLOAD; do
-  scp aliceml:Analysis/Run3Analysisvalidation/codeLF/$i .
+  env -i scp aliceml:Analysis/Run3Analysisvalidation/codeLF/$i .
 done
 
