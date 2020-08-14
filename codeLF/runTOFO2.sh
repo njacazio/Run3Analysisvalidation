4ad6d25a7d8f5a3e97a9514b5efc0c960a45f6b6
Nicolo' Jacazio <nicolo.jacazio@cern.ch>
2020-08-14 10:49:10 +0200

    Update O2 runners

--- codeLF/runTOFO2.sh
+++ codeLF/runTOFO2.sh
@@ -4,4 +4,5 @@ reset
 
 o2-analysis-spectra-tof --aod-file AO2D.root -b | o2-analysis-pid-tof -b
 
+rm localhost*
 
--- codeLF/runTPCO2.sh
+++ codeLF/runTPCO2.sh
@@ -9,4 +9,5 @@ reset
 o2-analysis-spectra-tpc --aod-file AO2D.root -b | o2-analysis-pid-tpc -b
 #o2-analysis-pid-tpc --aod-file AO2D.root -b 
 
+rm localhost*
 
