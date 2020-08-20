f851da68fa60251c398fdbabdb16846766c6afd1
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2020-08-20 13:04:56 +0200

    Update param maker

--- codeLF/paramTPC/parametrizeTPC.py
+++ codeLF/paramTPC/parametrizeTPC.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
 
-from utils import get_obj, draw
+from utils import get_obj, draw, TFile
 from ROOT import TGraphErrors, TCanvas, TF1
 from numpy import sqrt, power, log
-
-from ROOT import gROOT, TColor
-gROOT.LoadMacro("BB.h+g")
-from ROOT import BB, BB2
 from sys import argv
+import ROOT
+from ROOT import gROOT, TColor, TObjArray
 
+gROOT.LoadMacro("BB.h+g")
+from ROOT import BB, BB2, BBReso
 
 def makeparam(part="Pr", mass=0.938272):
-    h = get_obj("AnalysisResults_TPC.root",
+    h = get_obj("../AnalysisResults_TPC.root",
                 "htpcsignal" + part,
                 "TPCpidqa-signalwTOF-task/")
     can = TCanvas(part, part)
     draw(h, "COLZ")
     # h.GetZaxis().SetRangeUser(2, 100)
-    proj = h.ProfileX("profile", 1, -1, "o")
-    draw(proj, "same")
+    profile = h.ProfileX("profile", 1, -1, "o")
+    draw(profile, "same")
+
+    slices = TObjArray()
+    h.FitSlicesY(0, 0, -1, 0, "QNR", slices)
+    mean = slices.At(1)
+    sigma = slices.At(2)
+    draw(mean, "same")
+    draw(sigma, "same")
+    draw(BBReso(), "same")
 
     g = TGraphErrors()
     g.SetName("bg")
@@ -30,8 +38,8 @@ def makeparam(part="Pr", mass=0.938272):
         x /= mass
         ex = h.GetXaxis().GetBinWidth(i)
         ex /= mass
-        y = proj.GetBinContent(i)
-        ey = proj.GetBinError(i)
+        y = profile.GetBinContent(i)
+        ey = profile.GetBinError(i)
         g.SetPoint(g.GetN(), x, y)
         g.SetPointError(g.GetN()-1, ex, ey)
     gcan = TCanvas("g", "g")
@@ -41,12 +49,26 @@ def makeparam(part="Pr", mass=0.938272):
     draw(fun[1], "same")
     fun[2].SetLineColor(1)
     draw(fun[2], "same")
-
+    p = "{"
+    for i in range(fun[1].GetNpar()):
+        p += f"{fun[1].GetParameter(i)},"
+        params.append(fun[1].GetParameter(i))
+    p += "}"
+    p = p.replace(",}", "};")
+    print(f"std::vector<float> p = {p}")
     input()
-    g.SaveAs(f"/tmp/bg_{part}.root")
+    f = TFile(f"/tmp/bg_{part}.root", "RECREATE")
+    f.cd()
+    g.Write()
+    bb = ROOT.o2.pid.tpc.BetheBloch()
+    for i in range(0, bb.GetParameters().size()):
+        bb.SetParameter(i, fun[1].GetParameter(i))
+    bb.Write()
+    f.Close()
     return can, gcan
 
-def plotbg(part="Pi Ka Pr De"):
+
+def plotbg(part="Pi Ka Pr"):
     h = [get_obj(f"/tmp/bg_{i}.root", "bg", "") for i in part.split()]
     cols = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3']
     for i in enumerate(h):
@@ -55,17 +77,15 @@ def plotbg(part="Pi Ka Pr De"):
     opt = ""
     for i in h:
         draw(i, opt)
-        opt+="same"
+        opt += "same"
+    input()
     return can
-
 
 
 if __name__ == "__main__":
     pass
     # makeparam("Pi", 0.139570)
     # makeparam("Ka", 0.493677)
-    # makeparam(part="Pr", mass=0.938272)
-    makeparam(part="De", mass=1.875613)
-    c = plotbg()
-
-    input()
+    makeparam(part="Pr", mass=0.938272)
+    # makeparam(part="De", mass=1.875613)
+    # c = plotbg()
