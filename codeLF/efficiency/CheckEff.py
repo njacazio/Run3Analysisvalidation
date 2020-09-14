e62047937210709ae5c4e82d40a9154dd0f008b3
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2020-09-14 17:30:13 +0200

    Update eff maker

--- codeLF/efficiency/CheckEff.py
+++ codeLF/efficiency/CheckEff.py
@@ -51,7 +51,7 @@ def get_num_den(input_file, draw_src=False):
             dens[pname].Add(den.ProjectionX(pname + "_den" + input_file, i, i))
 
 
-def main(input_files, show=True):
+def main(input_files, show=False):
     if "-b" in input_files:
         input_files.remove("-b")
     for i in input_files:
@@ -60,16 +60,21 @@ def main(input_files, show=True):
     for i in nums:
         n = nums[i]
         d = dens[i]
-        effs.append(n.Clone("Eff_" + n.GetName().replace("_num", "")))
+        name = n.GetName().replace("_num", "")
+        effs.append(n.Clone("Eff_" + name))
+        effs[-1].SetTitle(name)
         effs[-1].Divide(n, d, 1, 1, "B")
     fout = TFile("/tmp/Eff.root", "RECREATE")
     fout.cd()
+    for i in nums:
+        nums[i].Write()
+        dens[i].Write()
     for i in effs:
         i.Write()
         if show:
             can = TCanvas(i.GetName(), i.GetName())
             frame = can.DrawFrame(
-                0, 0, 5, 1, ";#it{p}_{T} (GeV/#it{c});Efficiency")
+                0, 0, 5, 1, ";#it{p}_{T} (GeV/#it{c});Efficiency " + i.GetTitle())
             frame
             draw(i, "same")
             print(i)
