82e0d8b9c190f01ac9f4379fad5614f0fa0f488b
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2020-09-14 18:02:58 +0200

    Rename addtask

---
+++ codeLF/efficiency/AddTaskCheckMC.C
@@ -0,0 +1,43 @@
+TaskCheckMC* AddTaskCheckMC(const TString partName)
+{
+
+  /* check analysis manager */
+  AliAnalysisManager* mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {
+    Error("AddTaskCheckMC", "cannot get analysis manager");
+    return NULL;
+  }
+
+  /* check input event handler */
+  if (!mgr->GetInputEventHandler()) {
+    Error("AddTaskCheckMC", "cannot get input event handler");
+    return NULL;
+  }
+
+  /* check input data type */
+  TString str = mgr->GetInputEventHandler()->GetDataType();
+  if (str.CompareTo("ESD")) {
+    Error("AddTaskCheckMC", "input data type is not \"ESD\"");
+    return NULL;
+  }
+
+  /* check MC truth event handler */
+  if (!mgr->GetMCtruthEventHandler()) {
+    Error("AddTaskCheckMC", "cannot get MC truth event handler");
+    return NULL;
+  }
+
+  /* get common input data container */
+  AliAnalysisDataContainer* inputc = mgr->GetCommonInputContainer();
+  if (!inputc) {
+    Error("AddTaskCheckMC", "cannot get common input container");
+    return NULL;
+  }
+
+  /*  create task and connect input/output */
+  TaskCheckMC* task = new TaskCheckMC(partName, tag);
+  mgr->ConnectInput(task, 0, inputc);
+
+  /* return task */
+  return task;
+}
