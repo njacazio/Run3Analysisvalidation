b79fe065f796566fbfaf85885e2a7ed1af6732f4
Nicolo' Jacazio <nicolo.jacazio@cern.ch>
2020-09-21 15:55:00 +0200

    Add configuration for TPC benchmark

--- codeLF/Benchmark/TPCSpectraBenchmark/ConfigureTrain.C
+++ codeLF/Benchmark/TPCSpectraBenchmark/ConfigureTrain.C
@@ -1,11 +1,8 @@
 #include <string>
 
-#include <AliExternalTrackParam.h>
-
 #include <AliAnalysisTaskAO2Dconverter.h>
-#include <AliAnalysisTaskPIDResponse.h>
+#include <AliAnalysisTaskSpectraTPCRun3.h>
 #include <AliAnalysisTaskWeakDecayVertexer.h>
-#include <AliExternalTrackParam.h>
 #include <AliPhysicsSelectionTask.h>
 #include <TROOT.h>
 
@@ -16,13 +13,16 @@ void ConfigureTrain()
   gROOT->ProcessLine("AddTaskMultSelection()");
 
   // PhysicsSelection Configuration
-  gROOT->LoadMacro("$ALICE_PHYSICS/OADB/macros/AddTaskPhysicsSelection.C");
-  gROOT->ProcessLine("AddTaskPhysicsSelection(kTRUE)");
-  if (1) { // Conversion for Run3
+  // gROOT->LoadMacro("$ALICE_PHYSICS/OADB/macros/AddTaskPhysicsSelection.C");
+  // Use a reinterpreted cast to get the task for further configurations in this task
+  gROOT->ProcessLine("AddTaskPhysicsSelection(false, true)");
+
+  if (1) { // PID task
     // PID
     gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDResponse.C");
     gROOT->ProcessLine("AddTaskPIDResponse()");
-
+  }
+  if (0) { // Conversion for run 3
     // Weak decays
     gROOT->LoadMacro("$ALICE_PHYSICS/PWGLF/STRANGENESS/Cascades/Run2/macros/AddTaskWeakDecayVertexer.C");
     AliAnalysisTaskWeakDecayVertexer* decays = reinterpret_cast<AliAnalysisTaskWeakDecayVertexer*>(gROOT->ProcessLine("AddTaskWeakDecayVertexer()"));
@@ -33,11 +33,13 @@ void ConfigureTrain()
     // Converter
     gROOT->LoadMacro("$ALICE_PHYSICS/RUN3/AddTaskAO2Dconverter.C");
     AliAnalysisTaskAO2Dconverter* converter = reinterpret_cast<AliAnalysisTaskAO2Dconverter*>(gROOT->ProcessLine("AddTaskAO2Dconverter()"));
-    converter->SetMCMode();
     converter->SetUseEventCuts(true);
     converter->SetTruncation(true);
   }
-  gROOT->LoadMacro("TaskCheckMC.cxx+g");
-  gROOT->LoadMacro("AddTaskCheckMC.C");
-  gROOT->ProcessLine("AddTaskCheckMC(\"check\");");
+  if (1) { // Spectra task
+    gROOT->LoadMacro("$ALICE_PHYSICS/PWGLF/SPECTRA/PiKaPr/TPC/Run3/AddTaskSpectraTPCRun3.C");
+    // AliAnalysisTaskSpectraTPCRun3* AddTaskSpectraTPCRun3(Bool_t mc = kFALSE, Bool_t aod = kTRUE, Bool_t o2pid = kTRUE)
+    AliAnalysisTaskSpectraTPCRun3* bc = reinterpret_cast<AliAnalysisTaskSpectraTPCRun3*>(gROOT->ProcessLine("AddTaskSpectraTPCRun3(kFALSE, kTRUE, kFALSE)"));
+    bc->fUseEventSelection = kFALSE;
+  }
 }
