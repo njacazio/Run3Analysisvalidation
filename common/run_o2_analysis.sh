4ac207160ff7a4b5e1f930c4e2b8eb2721a0064d
Nicolo' Jacazio <nicolo.jacazio@cern.ch>
2020-09-29 21:34:02 +0200

    Update O2 task runner

--- common/run_o2_analysis.sh
+++ common/run_o2_analysis.sh
@@ -3,31 +3,34 @@
 reset
 
 function runit() {
-  cd $1
-  echo "PWD: ${@}"
-  pwd 
-  CMD="o2-analysis-mc-validation --aod-file AO2D.root -b | o2-analysis-trackselection -b"
-  echo $CMD
-  eval $CMD
-  cd -
+    cd $1
+    echo "PWD: ${@}"
+    pwd
+    CMD="o2-analysis-mc-validation --aod-file AO2D.root -b | o2-analysis-trackselection -b"
+    CMD="/home/njacazio/Analysis/Run3Analysisvalidation/codeLF/runTPCO2.sh"
+    echo $CMD
+    eval $CMD > run.log 2>&1
+    cd -
 }
 
 Path="${1}"
 if [[ -z "$Path" ]]; then
-  echo "Please provide data path"
-  exit 1
+    echo "Please provide data path"
+    exit 1
 fi
 
 Dirs=$(ls -d "$Path"/*)
 
 for i in $Dirs; do
-  runit "${i}" & 
+    echo $i
+    runit "${i}" &
+    # sleep 0.5
 done
 
 wait
 
-Files=$(find $1 | grep AnalysisResults.root)
-echo "Files:"
-echo $Files | xargs
+OUT_FILE="AnalysisResults_TPC.root"
+Files=$(find ${Path} | grep ${OUT_FILE} | xargs)
+echo "hadd /tmp/${OUT_FILE} $Files"
 
 
