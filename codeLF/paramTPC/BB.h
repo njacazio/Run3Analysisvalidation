e86dd66a8d75517cc49776e402ca01fd2dadaadd
Nicolo' Jacazio <nicolo.jacazio@cern.ch>
2020-09-29 11:37:28 +0200

    Update BB to have same of O2

--- codeLF/paramTPC/BB.h
+++ codeLF/paramTPC/BB.h
@@ -1,10 +1,11 @@
-#include "TF1.h"
-#include "TH1.h"
-#include "TColor.h"
-#include "TSystem.h"
-#include "TFile.h"
-#include "TPCSimulation/Detector.h"
 #include "PID/BetheBloch.h"
+#include "PID/TPCReso.h"
+#include "TColor.h"
+#include "TF1.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TPCSimulation/Detector.h"
+#include "TSystem.h"
 
 Double_t BetheBlochAleph(Double_t* x, Double_t* par)
 {
@@ -22,6 +23,13 @@ Double_t BetheBlochAleph2(Double_t* x, Double_t* par)
   //   Double_t charge = x[1];
   Double_t charge = 1;
   return par[5] * BetheBlochAleph(x, par) * std::pow(charge, par[6]);
+}
+
+o2::pid::tpc::BetheBloch* betheblocho2 = nullptr;
+Double_t BetheBlochO2(Double_t* x, Double_t* par)
+{
+  const float xx[2] = { (float)x[0], 1 };
+  return betheblocho2->operator()(xx);
 }
 
 TF1* BB(TString n = "BB")
@@ -52,6 +60,14 @@ TF1* BB2(TString n = "BB2")
   return f;
 }
 
+TF1* O2BB(TString n = "O2BB")
+{
+  gSystem->Exec("o2-ccdb-downloadccdbfile -p Analysis/PID/TPC/BetheBloch -d /tmp/ -t -1");
+  TFile fin("/tmp/Analysis/PID/TPC/BetheBloch/snapshot.root", "READ");
+  fin.GetObject("ccdb_object", betheblocho2);
+  return new TF1(n, BetheBlochO2, 0, 20, 7);
+}
+
 Double_t BBReso(Double_t* x, Double_t* par)
 {
   Double_t signal = x[0];
@@ -60,10 +76,25 @@ Double_t BBReso(Double_t* x, Double_t* par)
   return signal * par[0] * (points > 0 ? sqrt(1. + par[1] / points) : 1.f);
 }
 
+o2::pid::tpc::TPCReso* bbresoo2 = nullptr;
+Double_t BBResoO2(Double_t* x, Double_t* par)
+{
+  const float xx[2] = { (float)x[0], 90 };
+  return bbresoo2->operator()(xx);
+}
+
 TF1* BBReso(TString n = "BBReso")
 {
   TF1* f = new TF1(n, BBReso, 0, 20, 2);
   f->SetParameters(0.07, 0.0);
   f->SetLineColor(TColor::GetColor("#4daf4a"));
   return f;
+}
+
+TF1* O2BBReso(TString n = "O2BBReso")
+{
+  gSystem->Exec("o2-ccdb-downloadccdbfile -p Analysis/PID/TPC/TPCReso -d /tmp/ -t -1");
+  TFile fin("/tmp/Analysis/PID/TPC/TPCReso/snapshot.root", "READ");
+  fin.GetObject("ccdb_object", bbresoo2);
+  return new TF1(n, BBResoO2, 0, 20, 2);
 }
